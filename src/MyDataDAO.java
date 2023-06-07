5d3a9d22888d5381dc2c06a191f15e7ec94d4acd
benny <bennyz327@gmail.com>
2023-06-07 17:23:27 +0800

    實作新增、查詢、刪除功能
    需要再包裝

--- src/MyDataDAO.java
+++ src/MyDataDAO.java
@@ -1,9 +1,6 @@
 import ConnectionPack.ConnectionFactory;
 
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class MyDataDAO {
     static public boolean insertData(Mydata mdata){
@@ -31,5 +28,59 @@ public class MyDataDAO {
             return false;
         }
 
+    }//輸入的Mydata物件不能有空欄位值，因為SQL指令產生方式有缺陷
+    static public void deleteData(int id){
+        String sql = "DELETE FROM HospitalList WHERE id=?";
+        try(Connection conn = ConnectionFactory.getConn())  {
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1,id);
+            int count = pstmt.executeUpdate();
+            System.out.println(count+"筆資料受到影響");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }//done //根據id刪除資料 //todo 需要再包裝成使用者友好形式
+    static public Mydata selectData(int id){
+        String sql = "SELECT [ResourceAgency],[HospitalName],[PhoneNumber],[Fax],[Email],[Address],[Website],[xCoordinate],[yCoordinate],[Notes],[LastUpdateTime],[id] FROM HospitalList WHERE id=?";
+        try(Connection conn = ConnectionFactory.getConn())  {
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1,id);
+            ResultSet rs = pstmt.executeQuery();
+            Mydata rsMyData = null;
+            // 因為調閱主鍵，斷定返回結果集只有單筆資料或是沒有資料
+            if (rs.next()){
+                rsMyData = new Mydata();
+                rsMyData.setResourceAgency(rs.getString("ResourceAgency"));
+                rsMyData.setHospitalName(rs.getString("HospitalName"));
+                rsMyData.setPhoneNumber(rs.getInt("PhoneNumber"));
+                rsMyData.setFax(rs.getInt("Fax"));
+                rsMyData.setEmail(rs.getString("Email"));
+                rsMyData.setAddress(rs.getString("Address"));
+                rsMyData.setWebsite(rs.getString("Website"));
+                rsMyData.setxCoordinate(rs.getInt("xCoordinate"));
+                rsMyData.setyCoordinate(rs.getInt("yCoordinate"));
+                rsMyData.setNotes(rs.getString("Notes"));
+                rsMyData.setLastUpdateTime(rs.getDate("LastUpdateTime"));
+                return rsMyData;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("查詢錯誤");
+        return null;
+    }//done //根據id查詢資料，將整筆資料返回成Mydata物件
+    static public void updateData(int id,String field,Object value){
+        String sql = "UPDATE HospitalList SET "+field+" = ? WHERE id = ?";
+        try (Connection conn = ConnectionFactory.getConn();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setObject(1, value);
+            pstmt.setInt(2,id);
+            int rowsAffected = pstmt.executeUpdate();
+            System.out.println("Updated " + rowsAffected + " rows.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
+
+
