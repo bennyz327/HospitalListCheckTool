bdbcfb363bf0d1182f824437adfec030b54adce9
benny <bennyz327@gmail.com>
2023-06-07 12:41:38 +0800

    用來儲存單筆資料的載體

---
+++ src/Mydata.java
@@ -0,0 +1,104 @@
+import java.util.Date;
+public class Mydata {
+    Mydata(){super();}
+    private String resourceAgency;
+    private String hospitalName;
+    private int phoneNumber;
+    private int fax;
+    private String email;
+    private String address;
+    private String website;
+    private int xCoordinate;
+    private int yCoordinate;
+    private String notes;
+    private Date lastUpdateTime;
+
+    public String getResourceAgency() {
+        return resourceAgency;
+    }
+
+    public void setResourceAgency(String resourceAgency) {
+        this.resourceAgency = resourceAgency;
+    }
+
+    public String getHospitalName() {
+        return hospitalName;
+    }
+
+    public void setHospitalName(String hospitalName) {
+        this.hospitalName = hospitalName;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getFax() {
+        return fax;
+    }
+
+    public void setFax(int fax) {
+        this.fax = fax;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public int getxCoordinate() {
+        return xCoordinate;
+    }
+
+    public void setxCoordinate(int xCoordinate) {
+        this.xCoordinate = xCoordinate;
+    }
+
+    public int getyCoordinate() {
+        return yCoordinate;
+    }
+
+    public void setyCoordinate(int yCoordinate) {
+        this.yCoordinate = yCoordinate;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public Date getLastUpdateTime() {
+        return lastUpdateTime;
+    }
+
+    public void setLastUpdateTime(Date lastUpdateTime) {
+        this.lastUpdateTime = lastUpdateTime;
+    }
+
+}
